68114ae82b17f37e38f04d96eebc5448186edeb4
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2026-06-21 19:08:45 +0330

    create  user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    phone = models.CharField(max_length=20, unique=True)
+
+    is_active = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "phone"
+
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.phone
