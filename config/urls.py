fae4b686afb25bd240e160ad5c31fd387ee326e1
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2026-06-21 19:26:00 +0330

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/profile/", include("profiles.urls")),
+    path("api/wallet/", include("wallets.urls")),
 ]
