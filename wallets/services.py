8090a6c0477e2e45491cbde92a1b9031180e12b8
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2026-06-21 20:06:30 +0330

    Update services.py

--- wallets/services.py
+++ wallets/services.py
@@ -5,3 +5,28 @@ from django.db import transaction
 from ledger.services import create_balanced_journal
 
 from ledger.models import Account
+
+
+@transaction.atomic
+def deposit_money(user, amount):
+    amount = Decimal(str(amount))
+
+    wallet = user.wallet
+
+    bank_account = Account.objects.get(account_type="BANK")
+
+    user_account = Account.objects.get(user=user, account_type="USER_IRT")
+
+    create_balanced_journal(
+        description=f"Deposit {user.phone}",
+        entries=[
+            {"account": bank_account, "debit": amount},
+            {"account": user_account, "credit": amount},
+        ],
+    )
+
+    wallet.available_balance += amount
+
+    wallet.save()
+
+    return wallet
