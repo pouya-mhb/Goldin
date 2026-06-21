62fc5fde02deb027bed161b47b551b46c61afaed
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2026-06-21 19:26:13 +0330

    Update views.py

--- wallets/views.py
+++ wallets/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.response import Response
+
+from .serializers import WalletSerializer
+
+
+class WalletAPIView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+
+        serializer = WalletSerializer(request.user.wallet)
+
+        return Response(serializer.data)
