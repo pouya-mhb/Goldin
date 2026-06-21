42b5ca82c62427fe31cb0ae10731575234816d9a
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2026-06-21 20:20:59 +0330

    Update models.py

--- pricing/models.py
+++ pricing/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class GoldPrice(models.Model):
+
+    buy_price = models.DecimalField(max_digits=18, decimal_places=2)
+
+    sell_price = models.DecimalField(max_digits=18, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
