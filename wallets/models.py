379b1153540913dd1145edea8e6225fa2733fc77
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2026-06-21 23:22:13 +0330

    Update models.py

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,5 @@
+# wallets/models.py
+
 from django.db import models
 from django.conf import settings
 
@@ -8,13 +10,14 @@ class Wallet(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="wallet"
     )
 
+    # IRT balance
     available_balance = models.DecimalField(max_digits=18, decimal_places=2, default=0)
 
     locked_balance = models.DecimalField(max_digits=18, decimal_places=2, default=0)
 
+    # GOLD balance (grams)
+    available_gold = models.DecimalField(max_digits=18, decimal_places=4, default=0)
+
+    locked_gold = models.DecimalField(max_digits=18, decimal_places=4, default=0)
+
     created_at = models.DateTimeField(auto_now_add=True)
-
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user.phone
