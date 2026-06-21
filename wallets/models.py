0b06ca9ad095d64cc6c392b8f111e637c322d627
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2026-06-21 19:23:20 +0330

    Update models.py

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Wallet(models.Model):
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="wallet"
+    )
+
+    available_balance = models.DecimalField(max_digits=18, decimal_places=2, default=0)
+
+    locked_balance = models.DecimalField(max_digits=18, decimal_places=2, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.phone
