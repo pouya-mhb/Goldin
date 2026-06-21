3c32f81b5b1be98733da16593e0248abb57232c7
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2026-06-21 20:10:58 +0330

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Order(models.Model):
+
+    BUY = "BUY"
+    SELL = "SELL"
+
+    ORDER_TYPES = [
+        (BUY, BUY),
+        (SELL, SELL),
+    ]
+
+    PENDING = "PENDING"
+    COMPLETED = "COMPLETED"
+    FAILED = "FAILED"
+
+    STATUS_CHOICES = [
+        (PENDING, PENDING),
+        (COMPLETED, COMPLETED),
+        (FAILED, FAILED),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    order_type = models.CharField(max_length=10, choices=ORDER_TYPES)
+
+    grams = models.DecimalField(max_digits=18, decimal_places=4)
+
+    unit_price = models.DecimalField(max_digits=18, decimal_places=2)
+
+    total_price = models.DecimalField(max_digits=18, decimal_places=2)
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING)
+
+    created_at = models.DateTimeField(auto_now_add=True)
