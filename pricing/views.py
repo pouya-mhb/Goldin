53f5626fed2c0a64450a95d343a8558c6cd61851
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2026-06-22 14:50:53 +0330

    Update views.py

--- pricing/views.py
+++ pricing/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .services import get_current_price
+
+
+class CurrentPriceAPIView(APIView):
+
+    def get(self, request):
+
+        price = get_current_price()
+
+        return Response(
+            {
+                "buy_price": str(price.buy_price),
+                "sell_price": str(price.sell_price),
+            }
+        )
