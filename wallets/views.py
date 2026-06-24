ba0e0eecb3d9394f03167ee2e6c0cfb5340282d2
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2026-06-24 18:55:13 +0330

    bugfix

--- wallets/views.py
+++ wallets/views.py
@@ -1,21 +1,21 @@
-# wallets/views.py
-
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 
 class WalletAPIView(APIView):
 
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
 
         wallet = request.user.wallet
-        gold_wallet = request.user.wallet
 
         return Response(
             {
                 "irt_balance": wallet.available_balance,
                 "irt_locked": wallet.locked_balance,
-                "gold_balance": gold_wallet.available_gold,
-                "gold_locked": gold_wallet.locked_gold,
+                "gold_balance": wallet.available_gold,
+                "gold_locked": wallet.locked_gold,
             }
         )
